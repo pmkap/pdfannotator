6a5a29287803f448216a35a5b89ac5b36fcc482f
Peter Kaplan <peter@pkap.de>
2023-06-23 15:37:23 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,51 @@
+import io
+import os
+from typing import Annotated, Union
+
+from fastapi import FastAPI, Form, UploadFile
+from fastapi.responses import StreamingResponse
+from fpdf import FPDF
+from pypdf import PdfWriter
+
+app = FastAPI()
+
+@app.post("/annotate/")
+async def create_file(
+    file: UploadFile,
+    text: Annotated[str, Form()],
+    token: Annotated[Union[str, None], Form()] = None,
+):
+    if token !=  os.environ.get('PDFANNOTATE_TOKEN'):
+        return {"error": "wrong token"}
+
+    stream = annotate_pdf(file.file.read(), text)
+
+    headers = {
+        f'Content-Disposition': 'attachment; filename={file.filename}'
+    }
+    return StreamingResponse(stream, headers=headers, media_type="application/pdf")
+
+def annotate_pdf(in_bytes: bytes, text: str) -> io.BytesIO:
+    pdf = PdfWriter()
+    pdf.append(io.BytesIO(in_bytes))
+
+    page = pdf.get_page(0)
+    dim = [x/72*25.4 for x in page.mediabox[2:]]
+
+    overlay = FPDF()
+    overlay.add_page(format=dim)
+    overlay.set_font('helvetica', size=24)
+    overlay.set_text_color(255, 0, 0)
+    overlay.text(txt=text, x=80, y=10)
+
+    pdf2 = PdfWriter()
+    pdf2.append(io.BytesIO(overlay.output()))
+    overlay_page = pdf2.get_page(0)
+
+    page.merge_page(overlay_page)
+
+    b = io.BytesIO()
+    pdf.write(b)
+    b.seek(0)
+
+    return b
